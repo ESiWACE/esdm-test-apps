5782d521178b93b2db7fdca3327abc5a7f4f51d8
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-04-09 17:21:12 +0100

    Description to compile/run with ESDM.

--- aimes-dsl/src/runmpi.sh
+++ aimes-dsl/src/runmpi.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
+source ../../source.sh
+
 DTYPE=float
 BLOCK=64
 TILE=3
+GRID=1024
 echo "using $TILE-side tiled grid (Data type: $DTYPE, Block size: $BLOCK)"
-mpicc *.c -o a.out -DGVAL=$DTYPE -DNBRS=$TILE -DBLKSIZE=$BLOCK -O3 -lnetcdf -I/home/nabeeh/Desktop/tmp/netcdf-4.1.3/include # -fopenmp
-mpiexec -n 1  --map-by node --display-map  ./a.out -G=1024 -m=0 -M=1000 -T=10 -R=input.cdf -W=output.cdf
-#mpiexec -n 1  --map-by node --display-map  ./a.out -G=256 -m=0 -M=1000 -T=10 -R=test_gv_temp2 -W=output.cdf
 
+cp ../../esdm.conf .
+
+echo "Creating input file"
+mpicc tools/var_init.c option.c io_netcdf.c grid.c -o var_init -DGVAL=$DTYPE -DNBRS=$TILE -DBLKSIZE=$BLOCK -O3 $CFLAGS $LDFLAGS -lnetcdf
+
+mkesdm
+./var_init -G=$GRID -f=esdm://input.cdf
+
+mpicc *.c -o model -DGVAL=$DTYPE -DNBRS=$TILE -DBLKSIZE=$BLOCK -O3 -fopenmp $CFLAGS $LDFLAGS -lnetcdf
+
+echo "Running the model"
+mpiexec -n 2 --map-by node --display-map  ./model -G=$GRID -m=0 -M=1000 -T=10 -R=esdm://input.cdf -W=esdm://output.cdf
---
+++ source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "This file should be sourced"
+ESDM_INSTALL_DIR=/home/kunkel/ur-git/esiwace/ESD-Middleware/install/
+export PATH=$ESDM_INSTALL_DIR/bin:$PATH
+
+alias mkesdm="mkfs.esdm -g -l --create  --remove --ignore-errors"
+LDFLAGS="-Wl,--rpath=$ESDM_INSTALL_DIR/lib -L$ESDM_INSTALL_DIR/lib"
+CFLAGS="-I$ESDM_INSTALL_DIR/include"
