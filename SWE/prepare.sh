b381c88dff9e5918a0334fa95cda0cfc75a5fc2d
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-11-30 18:14:11 +0000

    Fix

--- SWE/prepare.sh
+++ SWE/prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/TUM-I5/SWE.git
+cd SWE
 git submodule update --init --recursive
+cd ..
 # Must remove EARLY MPI Finalize and add global size
 # sed -i "s#MPI_Finalize#//MPI_Finalize#" SWE/src/examples/swe_mpi.cpp
 cp swe_mpi.cpp SWE/src/examples/swe_mpi.cpp
