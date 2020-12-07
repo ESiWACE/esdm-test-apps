93ecab42d9c85479d258b7a5c2071e3ee7fec4f4
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-12-07 10:29:01 +0000

    Update

--- netcdf-bench/extract-performance.sh
+++ netcdf-bench/extract-performance.sh
@@ -4,11 +4,16 @@ function extract(){
   t=$1
   f=$2
 
+  perf=$(echo $(grep "$t *I/O Performance  " $f | cut -b "62-80") | sed "s/ /+/g")
+  if [[ "$perf" == "" ]] ; then
+    return
+  fi
+  plus=$(echo $perf | sed "s/[^+]//g" | wc -c)
   echo -n $(basename $f | sed 's/\(.*\)-\([^-]*\)-\([^-]*\).txt/\1 \2 \3/') | sed "s/.conf//"
   echo -n " $t "
   echo -n "("
-  echo -n $(grep "$t *I/O Performance  " $f | cut -b "62-80") | sed "s/ / + /g"
-  echo    ")/"
+  echo -n $perf
+  echo    ")/$plus"
 }
 
 for f in $@ ; do
